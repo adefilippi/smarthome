1278eef4c0385a6d1be3fc54c0c31312749077d8
adefilippi <adefilippi@agencedps.com>
2025-01-01 19:37:16 +0100

    first commit

---
+++ components/controls/colorpicker.vue
@@ -0,0 +1,464 @@
+<template>
+  <div
+      role="slider"
+      :aria-roledescription="ariaRoledescription"
+      :aria-label="ariaLabel"
+      :aria-expanded="isPaletteIn"
+      aria-valuemin="0"
+      aria-valuemax="359"
+      :aria-valuenow="angle"
+      :aria-valuetext="ariaValuetext || valuetext"
+      :aria-disabled="disabled"
+      class="rcp"
+      :tabindex="disabled ? -1 : 0"
+      :style="{ '--rcp-initial-angle': initialAngle }"
+      @keyup.enter="selectColor"
+      @keydown="onKeyDown"
+  >
+    <div class="rcp__palette" />
+
+    <div
+        class="rcp__rotator"
+        :style="{
+                'pointer-events': disabled || isPressed || !isKnobIn ? 'none' : null,
+            }"
+        v-on="mouseScroll ? { wheel: onScroll } : {}"
+        ref="rotator"
+    >
+      <div class="rcp__knob" :class="isKnobIn ? 'in' : 'out'" @transitionend="hidePalette"></div>
+    </div>
+
+    <div class="rcp__ripple" :class="{ rippling: isRippling }" :style="{ borderColor: color }"></div>
+
+    <button
+        type="button"
+        class="rcp__well"
+        :aria-label="ariaLabelColorWell"
+        :disabled="disabled"
+        :tabindex="disabled ? -1 : 0"
+        :class="{ pressed: isPressed }"
+        :style="{ backgroundColor: color }"
+        @animationend="togglePicker"
+        @click="selectColor"
+    ><Icon name="uil:lightbulb-alt" class="text-7xl" /></button>
+  </div>
+</template>
+
+<script>
+import { ref, computed, onMounted, onBeforeUnmount, watch } from 'vue';
+
+import Rotator from '@radial-color-picker/rotator';
+const colors = ['red', 'yellow', 'green', 'cyan', 'blue', 'magenta', 'red'];
+const keys = {
+  ArrowUp: (oldAngle, step) => oldAngle + step,
+  ArrowRight: (oldAngle, step) => oldAngle + step,
+  ArrowDown: (oldAngle, step) => oldAngle - step,
+  ArrowLeft: (oldAngle, step) => oldAngle - step,
+  PageUp: (oldAngle, step) => oldAngle + step * 10,
+  PageDown: (oldAngle, step) => oldAngle - step * 10,
+  Home: () => 0,
+  End: () => 359,
+};
+
+export default {
+  name: 'ColorPicker',
+  emits: ['select', 'input', 'change'],
+  props: {
+    hue: {
+      default: 0,
+    },
+    saturation: {
+      default: 100,
+    },
+    luminosity: {
+      default: 50,
+    },
+    alpha: {
+      default: 1,
+    },
+    step: {
+      default: 1,
+    },
+    mouseScroll: {
+      default: false,
+    },
+    variant: {
+      default: 'collapsible', // collapsible | persistent
+    },
+    disabled: {
+      default: false,
+    },
+    initiallyCollapsed: {
+      default: false,
+    },
+    ariaLabel: {
+      default: 'color picker',
+    },
+    ariaRoledescription: {
+      default: 'radial slider',
+    },
+    ariaValuetext: {
+      default: '',
+    },
+    ariaLabelColorWell: {
+      default: 'color well',
+    },
+  },
+  setup(props, { emit }) {
+    // template refs
+    const el = ref(null);
+    const rotator = ref(null);
+
+    // instance values
+    let rcp = null;
+
+    // state
+    const initialAngle = props.hue + 'deg';
+    const angle = ref(props.hue);
+    const isPaletteIn = ref(!props.initiallyCollapsed);
+    const isKnobIn = ref(!props.initiallyCollapsed);
+    const isPressed = ref(false);
+    const isRippling = ref(false);
+
+    const color = computed(
+        () => `hsla(${angle.value}, ${props.saturation}%, ${props.luminosity}%, ${props.alpha})`
+    );
+
+    const valuetext = computed(() => {
+      return colors[Math.round(angle.value / 60)];
+    });
+
+    watch(
+        () => props.hue,
+        (value) => {
+          angle.value = value;
+          rcp.angle = value;
+        }
+    );
+
+    // ignore testing code that will be removed by dead code elimination for production
+    /* v8 ignore next 5 */
+    if (process.env.NODE_ENV === 'development' && props.initiallyCollapsed && props.variant === 'persistent') {
+      console.warn(
+          `Incorrect config: using variant="persistent" and :initiallyCollapsed="true" at the same time is not supported.`
+      );
+    }
+
+    onMounted(() => {
+      // the Rorator module already has an extensive test suite
+      /* v8 ignore next 10 */
+      rcp = new Rotator(rotator.value, {
+        angle: angle.value,
+        onRotate(hue) {
+          angle.value = hue;
+          emit('input', angle.value);
+        },
+        onDragStop() {
+          emit('change', angle.value);
+        },
+      });
+    });
+
+    onBeforeUnmount(() => {
+      rcp.destroy();
+      rcp = null;
+    });
+
+    const onKeyDown = (ev) => {
+      if (props.disabled || isPressed.value || !isKnobIn.value || !(ev.key in keys)) return;
+
+      ev.preventDefault();
+
+      rcp.angle = keys[ev.key](rcp.angle, props.step);
+
+      angle.value = rcp.angle;
+      emit('input', angle.value);
+      emit('change', angle.value);
+    };
+
+    const onScroll = (ev) => {
+      if (isPressed.value || !isKnobIn.value) return;
+
+      ev.preventDefault();
+
+      if (ev.deltaY > 0) {
+        rcp.angle += props.step;
+      } else {
+        rcp.angle -= props.step;
+      }
+
+      angle.value = rcp.angle;
+      emit('input', angle.value);
+      emit('change', angle.value);
+    };
+
+    const selectColor = () => {
+      isPressed.value = true;
+
+      if (isPaletteIn.value && isKnobIn.value) {
+        emit('select', angle.value);
+        isRippling.value = true;
+      } else {
+        isPaletteIn.value = true;
+      }
+    };
+
+    const togglePicker = () => {
+      if (props.variant !== 'persistent') {
+        if (isKnobIn.value) {
+          isKnobIn.value = false;
+        } else {
+          isKnobIn.value = true;
+          isPaletteIn.value = true;
+        }
+      }
+
+      isRippling.value = false;
+      isPressed.value = false;
+    };
+
+    const hidePalette = () => {
+      if (!isKnobIn.value) {
+        isPaletteIn.value = false;
+      }
+      emit('state', isPaletteIn.value);
+    };
+
+    return {
+      // private API intented for testing memory leaks
+      rcp,
+
+      // refs
+      rotator,
+
+      // state
+      initialAngle,
+      angle,
+      isPaletteIn,
+      isKnobIn,
+      isRippling,
+      isPressed,
+
+      // computed
+      color,
+      valuetext,
+
+      // methods
+      onKeyDown,
+      onScroll,
+      selectColor,
+      togglePicker,
+      hidePalette,
+    };
+  },
+};
+</script>
+
+<style>
+/**
+ * The ColorPicker has 2 tab stops - one for the visual object that the user moves
+ * and one for the second type of interaction, i.e. selecting a color via the well
+ *
+ * @see https://www.w3.org/WAI/ARIA/apg/patterns/slider/
+ */
+.rcp,
+.rcp *,
+.rcp *:before {
+  -webkit-touch-callout: none;
+  -webkit-tap-highlight-color: transparent;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  user-select: none;
+  box-sizing: border-box;
+  border-radius: 50%;
+}
+
+.rcp,
+.rcp__knob,
+.rcp__ripple,
+.rcp__well {
+  aspect-ratio: 1;
+}
+
+.rcp {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 280px;
+  position: relative;
+  transform: scale(1.001);
+  transition: transform 0.15s cubic-bezier(0.68, 0, 0.47, 2);
+}
+
+.rcp:focus {
+  outline: 0;
+}
+
+.rcp:hover .rcp__knob {
+  box-shadow:
+      0 0 20px rgba(0, 0, 0, 0.19),
+      0 0 10px rgba(0, 0, 0, 0.24);
+}
+
+/* Allow tapping through the transparent area surrounding the well when collapsed */
+.rcp[aria-expanded='false'] {
+  pointer-events: none;
+}
+
+.rcp[aria-expanded='false'] .rcp__well {
+  pointer-events: auto;
+}
+
+
+.rcp[aria-disabled='true'] {
+  cursor: not-allowed;
+  transform: scale(0.96);
+}
+
+.rcp__palette {
+  position: absolute;
+  inset: 0;
+  background-size: 100% 100%;
+  background-image: conic-gradient(red, yellow, lime, aqua, blue, magenta, red);
+  -webkit-mask-image: radial-gradient(circle at 50% 50%, transparent 30%);
+  mask-image: radial-gradient(circle at 50% 50%, transparent 30%);
+  will-change: transform, opacity;
+  transition:
+      transform 0.5s cubic-bezier(0.35, 0, 0.25, 1),
+      opacity 0.5s cubic-bezier(0.35, 0, 0.25, 1);
+  transform: scale(1);
+  opacity: 1;
+}
+
+.rcp[aria-expanded='false'] .rcp__palette {
+  transform: scale(0);
+  opacity: 0;
+}
+
+.rcp[aria-disabled='true'] .rcp__palette {
+  filter: contrast(0.25);
+}
+
+.rcp__rotator {
+  position: absolute;
+  inset: 0;
+  transform: rotate(var(--rcp-initial-angle));
+}
+
+.rcp__knob {
+  box-shadow:
+      0 0 10px rgba(0, 0, 0, 0.12),
+      0 0 5px rgba(0, 0, 0, 0.16);
+  width: 15%;
+
+  /*
+   * "2.5%" adds distance from the top edge of the rotator to center the knob inside the gradient ring
+   * "auto" centers the knob horizontally inside the rotator plane
+   */
+  margin-top: 2.5%;
+  margin-inline: auto;
+
+  background-color: #fff;
+  transition: transform 0.4s cubic-bezier(0.35, 0, 0.25, 1);
+  outline: 0;
+  border-style: none;
+}
+
+.rcp__knob.in {
+  transform: scale(1);
+}
+
+.rcp__knob.out {
+  transform: scale(0);
+}
+
+.rcp[aria-disabled='true'] .rcp__knob {
+  box-shadow: none;
+  pointer-events: none;
+}
+
+.rcp__well {
+  width: 40%;
+  padding: 0;
+  margin: 0;
+  background-color: #ff0000;
+  outline: 0;
+  cursor: pointer;
+  border: 1px solid #b2b2b2;
+  z-index: 1;
+  position: relative;
+}
+
+.rcp__well:before {
+  content: '';
+  position: absolute;
+  display: block;
+  inset: 0;
+  border: 6px solid #fff;
+}
+
+.rcp__well::-moz-focus-inner {
+  border: 0;
+}
+
+.rcp:focus-visible .rcp__knob,
+.rcp__well:focus-visible {
+  box-shadow: 0 0 0 2px rgba(160, 174, 192, 0.6);
+}
+
+.rcp__well:hover {
+  border-color: #6b7280;
+}
+
+.rcp__well.pressed {
+  animation: rcp-beat 0.4s cubic-bezier(0.35, 0, 0.25, 1) forwards;
+}
+
+.rcp[aria-disabled='true'] .rcp__well {
+  background-color: #bfbfbf !important;
+  pointer-events: none;
+}
+
+.rcp__ripple {
+  width: 20%;
+  opacity: 1;
+  position: absolute;
+
+  /* red is just a default initial value which matches the red hue at 0º */
+  border: 8px solid #ff0000;
+}
+
+.rcp__ripple.rippling {
+  animation: rcp-ripple 0.5s cubic-bezier(0.35, 0, 0.25, 1) forwards;
+}
+
+@keyframes rcp-ripple {
+  0% {
+    transform: scale(1);
+    opacity: 1;
+  }
+  50% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+    border-width: 0;
+    transform: scale(3.8);
+  }
+}
+
+@keyframes rcp-beat {
+  0% {
+    transform: scale(1);
+  }
+  25% {
+    transform: scale(0.8);
+  }
+  50% {
+    transform: scale(1);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+</style>
---
+++ components/lightSwitch.vue
@@ -0,0 +1,220 @@
+<template>
+  <div class="h-96 ">
+    <div class="text-3xl text-center my-6">{{ device.name }}</div>
+    <div class="flex justify-center items-center flex-col">
+      <div class="light-switch">
+        <colorpicker :hue="hue" saturation="100" luminosity="50" :initiallyCollapsed="!on" @state="updateDeviceState"
+                     @change="updateColor"></colorpicker>
+
+        <div class="custom-slider mt-4 h-8" :style="{ '--ProgressPercent': luminosity + '%', '--ProgressColor': color }">
+          <input v-model="luminosity" type="range" min="0" max="100" class="slider" v-if="on"/>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import colorpicker from "~/components/controls/colorpicker.vue";
+
+const emit = defineEmits(['stateChanged'])
+
+
+const on = ref(true)
+const color = ref('#FF0000')
+const luminosity = ref(20)
+const hue = ref(0)
+
+const props = defineProps({
+  device: {
+    default: null
+  },
+})
+watch(luminosity, (newValue) => {
+  emit('stateChanged', {
+    id: props.device.id,
+    call: 'brightness',
+    value: newValue,
+  });
+});
+
+if (props.device !== null) {
+  const o = props.device.values.filter((v) => v.name === 'switchable-power')[0]
+  on.value = o.value
+
+  const d = props.device.values.filter((v) => v.name === 'dimmable')[0]
+  luminosity.value = d.value
+
+  const c = props.device.values.filter((v) => v.name === 'color')[0]
+  color.value = c.value
+
+  const {h} = hexToHSL(color.value)
+  hue.value = h
+}
+
+function updateDeviceState(state) {
+  on.value = state
+  emit('stateChanged', {
+    id: props.device.id,
+    call: 'power',
+    value: on.value,
+  });
+}
+
+function updateColor(c) {
+  color.value = HSLToHex(c, 100, 50) // update color
+  emit('stateChanged', {
+    id: props.device.id,
+    call: 'color',
+    value: color.value,
+  });
+}
+
+
+// Convertir Hex en HSL
+function hexToHSL(hex) {
+  // Enlever le # si présent
+  hex = hex.replace(/^#/, '');
+
+  // Convertir en RGB
+  let r = parseInt(hex.slice(0, 2), 16) / 255;
+  let g = parseInt(hex.slice(2, 4), 16) / 255;
+  let b = parseInt(hex.slice(4, 6), 16) / 255;
+
+  // Trouver min et max
+  let max = Math.max(r, g, b);
+  let min = Math.min(r, g, b);
+
+  let h, s, l = (max + min) / 2;
+
+  if (max === min) {
+    h = s = 0; // achromatique
+  } else {
+    let d = max - min;
+    s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+
+    switch (max) {
+      case r:
+        h = (g - b) / d + (g < b ? 6 : 0);
+        break;
+      case g:
+        h = (b - r) / d + 2;
+        break;
+      case b:
+        h = (r - g) / d + 4;
+        break;
+    }
+    h /= 6;
+  }
+
+  // Convertir en degrés et pourcentages
+  h = Math.round(h * 360);
+  s = Math.round(s * 100);
+  l = Math.round(l * 100);
+
+  return {h, s, l};
+}
+
+// Convertir HSL en Hex
+function HSLToHex(h, s, l) {
+  // Convertir en décimales
+  s /= 100;
+  l /= 100;
+  h = h % 360;
+
+  function hue2rgb(p, q, t) {
+    if (t < 0) t += 1;
+    if (t > 1) t -= 1;
+    if (t < 1 / 6) return p + (q - p) * 6 * t;
+    if (t < 1 / 2) return q;
+    if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+    return p;
+  }
+
+  let r, g, b;
+
+  if (s === 0) {
+    r = g = b = l; // achromatique
+  } else {
+    let q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+    let p = 2 * l - q;
+
+    r = hue2rgb(p, q, (h / 360) + 1 / 3);
+    g = hue2rgb(p, q, h / 360);
+    b = hue2rgb(p, q, (h / 360) - 1 / 3);
+  }
+
+  // Convertir en hex
+  const toHex = x => {
+    const hex = Math.round(x * 255).toString(16);
+    return hex.length === 1 ? '0' + hex : hex;
+  };
+
+  return `#${toHex(r)}${toHex(g)}${toHex(b)}`;
+}
+
+
+</script>
+<style scoped>
+.light-switch {
+  width: 280px;
+}
+
+.custom-slider {
+  --trackHeight: 2rem;
+  --thumbRadius: 2rem;
+  width: 100%;
+}
+
+/* style the input element with type "range" */
+.custom-slider input[type="range"] {
+  position: relative;
+  appearance: none;
+  /* pointer-events: none; */
+  border-radius: 999px;
+  z-index: 0;
+  width: 100%;
+}
+
+/* ::before element to replace the slider track */
+.custom-slider input[type="range"]::before {
+  content: "";
+  position: absolute;
+  width: var(--ProgressPercent, 50%);
+  height: 100%;
+  background: var(--ProgressColor, #AAAAAA);
+  /* z-index: -1; */
+  pointer-events: none;
+  border-radius: 999px;
+}
+
+
+/* `::-webkit-slider-runnable-track` targets the track (background) of a range slider in chrome and safari browsers. */
+.custom-slider input[type="range"]::-webkit-slider-runnable-track {
+  appearance: none;
+  background: #AAAAAA;
+  height: var(--trackHeight);
+  border-radius: 999px;
+}
+
+/* `::-moz-range-track` targets the track (background) of a range slider in Mozilla Firefox. */
+.custom-slider input[type="range"]::-moz-range-track {
+  appearance: none;
+  background: #AAAAAA;
+  height: var(--trackHeight);
+  border-radius: 999px;
+}
+
+.custom-slider input[type="range"]::-webkit-slider-thumb {
+  position: relative;
+  top: 50%;
+  transform: translate(0, -50%);
+  width: var(--thumbRadius);
+  height: var(--thumbRadius);
+  background: transparent;
+  border-radius: 999px;
+  pointer-events: all;
+  appearance: none;
+  z-index: 1;
+}
+</style>
